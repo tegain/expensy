7d70c44006f8fb6c5854186f223472221faa9ad6
Thomas Egain <thomas.egain@gmail.com>
2019-02-03 14:41:44 +0100

    feat: add joi for data validation

--- src/main.ts
+++ src/main.ts
@@ -9,7 +9,7 @@ MongoConnect(process.env.DB_URL)
   .then(() => {
     app.listen(PORT, (err: Error) => {
       if (err) return console.log(err);
-      return console.log(`[App::Info] Server is running at http://localhost:%s`, PORT);
+      console.log(`[App::Info] Server is running at http://localhost:%s`, PORT);
     });
   })
   .catch((err) => console.log(err));
